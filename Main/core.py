a6816402841203db752a3dd7d729e5baa1e4379b
Keruve <acarrascobru@gmail.com>
2024-04-27 09:38:15 +0100

    Main

---
+++ Main/core.py
@@ -0,0 +1,6 @@
+import funciones_parking as funpark
+
+while True:
+    funpark.clear_console()
+    funpark.main_menu()
+    funpark.selector_main_menu()
---
+++ Main/funciones_parking.py
@@ -0,0 +1,102 @@
+import os
+
+def guiongen():
+    print("-------------------------------------------------------------------------")
+def clear_console():
+    if os.name == 'nt':
+        _ = os.system('cls')
+    else:
+        _ = os.system('clear')
+def main_menu():
+    print("------------------------------ PARKING-001 ------------------------------")
+    print("1. Gestion de plazas disponibles")
+    print("2. Gestion de plazas ocupadas")
+    print("3. Gestion de plazas por tipo de vehiculo")
+    print("4. Gestion de plazas por planta")
+    print("5. Generacion ticket estacionamiento.")
+    print("6. Registro de clientes.")
+    print("7. Facturacion y registro de pagos.")
+    print("1. ")
+    print("1. ")
+    print("1. ")
+    print("1. ")
+    guiongen()
+    print("E. Cerrar el programa.")
+    guiongen()
+def selector_main_menu():
+    optiongen = str(input(">"))
+    option = optiongen.lower()
+    while True:
+        if option == "1":
+            clear_console()
+            print("---------------------- GESTION PLAZAS DISPONIBLES -----------------------")
+            input(">")
+        if option == "2":
+            clear_console()
+            print("---------------------- GESTION DE PLAZAS OCUPADAS -----------------------")
+            input(">")
+        if option == "3":
+            clear_console()
+            print("---------------- GESTION DE PLAZAS POR TIPO DE VEHICULO -----------------")
+            input(">")
+        if option == "4":
+            clear_console()
+            print("--------------------- GESTION DE PLAZAS POR PLANTA ----------------------")
+            gestion_plazas_planta()
+        if option == "5":
+            clear_console()
+            print("---------------- GENERACION DE TICKET DE ESTACIONAMIENTO ----------------")
+            input(">")
+        if option == "6":
+            clear_console()
+            print("------------------------- REGISTRO DE CLIENTES --------------------------")
+            input(">")
+        if option == "7":
+            clear_console()
+            print("-------------------- FACTURACION Y REGISTRO DE PAGOS --------------------")
+            input(">")
+        if option == "e":
+            clear_console()
+            print("Gracias por utilizar funpark :)")
+            breakpoint()
+        else:
+            clear_console()
+            print("------------------------------ ERROR ------------------------------------")
+            input("Porfavor ingrese una opcion valida, Presiona enter para continuar...")
+            continue
+def gestion_plazas_planta():
+    print("1. Planta 0")
+    print("1. Planta 1")
+    print("1. Planta 2")
+    print("1. Planta 3")
+    print("1. Planta VIP")
+    guiongen()
+    print("E. Para retroceder en el menu.")
+    guiongen()
+    optiongen = str(input(">"))
+    option = optiongen.lower()
+    while True:
+        if option == "1":
+            clear_console()
+            print("------------------------------- PLANTA 0 --------------------------------")
+        if option == "1":
+            clear_console()
+            print("------------------------------- PLANTA 1 --------------------------------")
+        if option == "1":
+            clear_console()
+            print("------------------------------- PLANTA 2 --------------------------------")
+        if option == "1":
+            clear_console()
+            print("------------------------------- PLANTA 3 --------------------------------")
+        if option == "1":
+            clear_console()
+            print("------------------------------ PLANTA VIP -------------------------------")
+        if option == "e":
+            clear_console()
+            main_menu()
+            selector_main_menu()
+        else:
+            clear_console()
+            print("------------------------------ ERROR ------------------------------------")
+            input("Porfavor ingrese una opcion valida, Presiona enter para continuar...")
+            continue
