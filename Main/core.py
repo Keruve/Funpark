ac9918477fcfb2fc8c8ade944d3e718a75753ad0
Keruve <165384283+Keruve@users.noreply.github.com>
2024-05-08 10:58:49 +0000

    v.001

--- Main/core.py
+++
@@ -1,8 +0,0 @@
-import funciones_parking as funpark
-from funciones_parking import ParkingSpot, Vehicle, Parking
-
-
-while True:
-    funpark.clear_console()
-    funpark.main_menu()
-    funpark.selector_main_menu()
